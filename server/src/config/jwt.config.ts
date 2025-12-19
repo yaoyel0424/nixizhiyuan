c62fb370dc67523942546b2e3dec002d1c038fa4
WANDOU\YBC <yaoyel@126.com>
2025-12-19 14:41:44 +0800

    修改jwt secretkey和expries time

--- server/src/config/jwt.config.ts
+++ server/src/config/jwt.config.ts
@@ -4,9 +4,9 @@ import { registerAs } from '@nestjs/config';
  * JWT 配置
  */
 export default registerAs('jwt', () => ({
-  secret: process.env.JWT_SECRET || 'your-secret-key',
-  expiresIn: process.env.JWT_EXPIRES_IN || '15m',
-  refreshSecret: process.env.JWT_REFRESH_SECRET || 'your-refresh-secret-key',
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+  secret: process.env.JWT_SECRET || '4e7ee6b641b3354834c475942f66b4f687242190b0b17fe999cde562027daf36',
+  expiresIn: process.env.JWT_EXPIRES_IN || '15d',
+  refreshSecret: process.env.JWT_REFRESH_SECRET || '305d8a7d3c9a492e18f3ee945637302dc86af6e12badfd436fcc12917688cfa0',
+  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '30d',
 }));
 
